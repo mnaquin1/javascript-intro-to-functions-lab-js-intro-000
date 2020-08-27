a18dab8b8532d84a916290ebe86fbf89859d286a
Michael Naquin <mnaquin1@yahoo.com>
2020-08-27 19:34:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,19 @@ function sayHiToGrandma(string){
 return string.toLowerCase("I can't hear you!")
 return string.toUpperCase('YES INDEED!')
 
+var uppercase = "HELLO!"
 
+uppercase.toUpperCase() === uppercase // true
+
+var lowercase = "I can'r hear you"
+
+lowercase.toLowerCase() === lowercase // true
+
+var mixedCase = 'Hi there!'
+
+mixedCase.toLowerCase() === mixedCase // false
+
+mixedCase.toUpperCase() === mixedCase // false
 
 
 
