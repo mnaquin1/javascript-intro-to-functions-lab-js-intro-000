01c7fbc6c2d7d0d3c29b77ec194cbe31199f9c93
Michael Naquin <mnaquin1@yahoo.com>
2020-08-27 20:03:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-var uppercase = "HELLO!"
-
-uppercase.toUpperCase() === uppercase // true
-
-var lowercase = 'hello!'
-
-lowercase.toLowerCase() === lowercase // true
-
-var mixedCase = 'Hi there!'
-
-mixedCase.toLowerCase() === mixedCase // false
-
-mixedCase.toUpperCase() === mixedCase // false
 function shout(string) {
 return string.toUpperCase()
 }
@@ -32,10 +19,18 @@ function sayHiToGrandma(string){
 return string.toLowerCase()
 
 return string.toUpperCase()
-
-
-
-
-
-
 }
+
+var uppercase = "HELLO!"
+
+uppercase.toUpperCase() === uppercase // true
+
+var lowercase = 'hello!'
+
+lowercase.toLowerCase() === lowercase // true
+
+var mixedCase = 'Hi there!'
+
+mixedCase.toLowerCase() === mixedCase // false
+
+mixedCase.toUpperCase() === mixedCase // false
