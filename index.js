03c1c3a9af009afc1f604a931b1311e0379f4e92
Michael Naquin <mnaquin1@yahoo.com>
2020-08-27 20:10:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,38 +17,10 @@ console.log(string.toLowerCase())
 
 function sayHiToGrandma(string){
 
-describe('sayHiToGrandma(string)', function() {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-  })
-
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-  })
-
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-  })
-})
-
 return string.toLowerCase()
 
 return string.toUpperCase()
 
 return string.toMixedCase()
 
-
-var uppercase = "HELLO!"
-
-string.toUpperCase() === uppercase // true
-
-var lowercase = 'hello!'
-
-string.toLowerCase() === lowercase // true
-
-var mixedCase = 'Hi there!'
-
-mixedCase.toLowerCase() === mixedCase // false
-
-mixedCase.toUpperCase() === mixedCase // false
 }
