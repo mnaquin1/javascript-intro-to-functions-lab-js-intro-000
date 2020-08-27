bce670638e452579a94012e83b2e6fb83b910ad4
Michael Naquin <mnaquin1@yahoo.com>
2020-08-27 20:07:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -36,3 +36,17 @@ var mixedCase = 'Hi there!'
 mixedCase.toLowerCase() === mixedCase // false
 
 mixedCase.toUpperCase() === mixedCase // false
+
+describe('sayHiToGrandma(string)', function() {
+  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
+  })
+
+  it('returns "YES INDEED!" if `string` is uppercase', function() {
+    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+  })
+
+  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+  })
+})
