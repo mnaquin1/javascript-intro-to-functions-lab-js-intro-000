200a9fc82c1c5b0b0fde4143b3cdc619002a76e6
Michael Naquin <mnaquin1@yahoo.com>
2020-08-27 19:29:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,3 +19,17 @@ return string.toUpperCase('YES INDEED!')
 return string.toLowerCase("I can't hear you!")
 
 }
+
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // false
+ 
+mixedCase.toUpperCase() === mixedCase // false
